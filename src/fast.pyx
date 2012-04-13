a36a163103f64cd4415d33249cd62d2b0bfef5ee
Jeremy Stober <stober@gmail.com>
2012-04-12 23:41:47 -0500

    Added 1d version with more optimization.

--- src/fast.pyx
+++ src/fast.pyx
@@ -12,7 +12,6 @@ cdef double min3(double a, double b, double c):
         m = c
     return m
 
-
 def dtw_fast(np.ndarray s, np.ndarray t):
     
     cdef int nrows = s.shape[0]
@@ -30,6 +29,28 @@ def dtw_fast(np.ndarray s, np.ndarray t):
     for i in range(nrows):
         for j in range(ncols):
             cost = la.norm(s[i] - t[j])
+            dtw[i+1,j+1] = cost + min3(dtw[i,j+1],dtw[i+1,j],dtw[i,j])
+
+    return dtw[nrows,ncols]
+
+# very fast
+def dtw_fast_1d(np.ndarray[DTYPE_t,ndim=1] s, np.ndarray[DTYPE_t,ndim=1] t):
+    
+    cdef int nrows = s.shape[0]
+    cdef int ncols = t.shape[0]
+    
+    cdef np.ndarray[DTYPE_t,ndim=2] dtw = np.zeros((nrows+1,ncols+1), dtype = DTYPE)
+
+    dtw[:,0] = 1e6
+    dtw[0,:] = 1e6
+    dtw[0,0] = 0.0
+    
+    cdef unsigned int i,j
+    cdef DTYPE_t cost
+    
+    for i in range(nrows):
+        for j in range(ncols):
+            cost = abs(s[i] - t[j])
             dtw[i+1,j+1] = cost + min3(dtw[i,j+1],dtw[i+1,j],dtw[i,j])
 
     return dtw[nrows,ncols]
